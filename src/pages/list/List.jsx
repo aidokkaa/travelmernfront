2bd823ad5ee70c527a813fb29f29657925e267c4
Aida <aida.ms0097@gmail.com>
2024-02-01 18:03:21 +0600

    first commit

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -19,7 +19,7 @@ export const List = () => {
   const [min,setMin] = React.useState(undefined);
   const [max,setMax] = React.useState(undefined)
   const { data, loading, error, reFetch } = useFetch(
-    `${process.env.REACT_APP_API_URL}hotels?city=${destination}`);
+    `${process.env.REACT_APP_API_URL}/hotels?city=${destination}`);
     const handleClick=()=>{
       reFetch()
     }
